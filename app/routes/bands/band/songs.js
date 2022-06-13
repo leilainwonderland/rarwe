ddff6c2ba865051a2c6af5001f224dcdfe572cc1
leilainwonderland <leila.nunez98@gmail.com>
2022-06-13 11:25:53 +0200

    CHAPTER 6

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,8 +1,9 @@
-import Route from '@ember/routing/route'
+import Route from '@ember/routing/route';
 
 export default class BandsBandSongsRoute extends Route {
   model() {
     let band = this.modelFor('bands.band');
+    console.log(band);
     return band.songs;
   }
 }
