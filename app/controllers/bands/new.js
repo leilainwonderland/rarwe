e35471df0fbdfab0e599978d841933fdf2de567b
leilainwonderland <leila.nunez98@gmail.com>
2022-06-21 22:55:40 +0200

    Fetching data from the back-end c-9

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,13 +1,14 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { dasherize } from '@ember/string';
 import { tracked } from '@glimmer/tracking';
 import Band from 'rarwe/models/band';
 import { service } from '@ember/service';
+import fetch from 'fetch';
 
 export default class BandsNewController extends Controller {
   @service catalog;
   @service router;
+  
   @tracked name;
 
   get hashNoName() {
@@ -20,9 +21,25 @@ export default class BandsNewController extends Controller {
   }
 
   @action
-  saveBand() {
-    let band = new Band({ name: this.name, id: dasherize(this.name) });
-    this.catalog.add('band', band);
-    this.router.transitionTo('bands.band.songs', band.id);
-  }
-}
+  async saveBand() {
+    let response = await fetch('/bands', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify({
+        data: {
+          type: 'bands',
+          attributes: {
+            name: this.name,
+          },
+        },
+      }),
+    });
+    let json = await response.json();
+    let { id, attributes } = json.data;
+    let record = new Band({ id, ...attributes });
+    this.catalog.add('band', record);
+    this.router.transitionTo('bands.band.songs', id);
+  };
+};
--- app/services/catalog.js
+++ app/services/catalog.js
@@ -1,14 +1,69 @@
 import Service from '@ember/service';
+import Band from 'rarwe/models/band';
+import Song from 'rarwe/models/song';
+import fetch from 'fetch'
 import { tracked } from 'tracked-built-ins';
+
+function extractRelationships(object) {
+  let relationships = {};
+  for (let relationshipsName in object) {
+    relationships[relationshipsName] = object[relationshipsName].links.related;
+  };
+  return relationships;
+};
 
 export default class CatalogService extends Service {
   storage = {};
-
   constructor() {
     super(...arguments);
     this.storage.bands = tracked([]);
     this.storage.songs = tracked([]);
-  }
+  };
+
+  async fetchAll(type) {
+    if (type === 'bands') {
+      let response = await fetch('/bands');
+      let json = await response.json();
+      // for (let item of json.data) {
+      //   let { id, attributes, relationships } = item;
+      //   let rels = extractRelationships(relationships);
+      //   let record = new Band({ id, ...attributes}, rels);
+      //   this.add('band', record);
+      // };
+      this.loadAll(json);
+      return this.bands;
+    };
+    if (type === 'songs') {
+      let response = await fetch('/songs');
+      let json = await response.json();
+      this.loadAll(json);
+      return this.songs;
+    };
+  };
+
+  loadAll(json) {
+    let records = [];
+    for (let item of json.data) {
+      records.push(this._loadResource(item));
+    };
+    return records;
+  };
+
+  _loadResource(data) {
+    let record;
+    let { id, type, attributes, relationships } = data;
+    if (type === 'bands') {
+      let rels = extractRelationships(relationships);
+      record = new Band({ id, ...attributes }, rels);
+      this.add('band', record);
+    };
+    if (type === 'songs') {
+      let rels = extractRelationships(relationships);
+      record = new Song({ id, ...attributes }, rels);
+      this.add('song', record);
+    };
+    return record;
+  };
 
   add(type, record) {
     let collection = type === 'band' ? this.storage.bands : this.storage.songs;
