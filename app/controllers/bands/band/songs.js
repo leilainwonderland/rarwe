c9c18a952bc801e290b09cf8c1c98bccf891b4f1
leilainwonderland <leila.nunez98@gmail.com>
2022-06-22 16:17:24 +0200

    Creation of model instances, and saving them to the back-end

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,13 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import Song from 'rarwe/models/song';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import fetch from 'fetch'
 
 export default class BandsBandSongsController extends Controller {
-    @tracked showAddSong = true;
-    @tracked title = '';
+  @tracked showAddSong = true;
+  @tracked title = '';
 
   get hashNoTitle() {
     return !this.title;
@@ -15,18 +14,18 @@ export default class BandsBandSongsController extends Controller {
 
   @service catalog;
 
-  @action async upDateRating( song, rating ) {
+  @action async upDateRating(song, rating) {
     song.rating = rating;
     let payload = {
       data: {
         id: song.id,
-        type:'songs',
+        type: 'songs',
         attributes: {
           rating
         },
       },
     };
-    await fetch (`/songs/${song.id}`, {
+    await fetch(`/songs/${song.id}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/vnd.api+json'
@@ -40,37 +39,12 @@ export default class BandsBandSongsController extends Controller {
     this.title = e.target.value;
   }
 
-  @action 
-  async saveSong () {
-    let payload = {
-      data: {
-        type: 'songs',
-        attributes: { title: this.title },
-        relationships: {
-          band: {
-            data: {
-              id: this.model.id,
-              type: 'bands'
-            },
-          },
-        },
-      },
-    };
-    let response = await fetch('/songs', {
-      method:'POST',
-      headers: {
-        'Content-Type': 'application/vnd.api+json'
-      },
-      body: JSON.stringify(payload)
-    });
-    let json = await response.json();
-    let { id, attributes, relationships } = json.data;
-    let rels = {};
-    for (let relationshipsName in relationships) {
-      rels[relationshipsName].links.related;
-    };
-    let song = new Song ({ id, ...attributes }, rels);s
-    this.catalog.add('song', song);
+  @action
+  async saveSong() {
+    let song = await this.catalog.create('song',
+      { title: this.title },
+      { band: { data: { id: this.model.id, type: 'bands' } } }
+    );
     this.model.songs = [...this.model.songs, song];
     this.title = '';
     this.showAddSong = true;
