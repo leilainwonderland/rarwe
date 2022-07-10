733c4630d7531d5e15c78488b108f0388d3cae5b
leilainwonderland <leila.nunez98@gmail.com>
2022-07-10 16:33:21 +0200

    CHAPTER 12: Filtering song by a search term

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -4,10 +4,32 @@ import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class BandsBandSongsController extends Controller {
-  @service catalog;
 
   @tracked showAddSong = true;
   @tracked title = '';
+  @tracked sortBy = 'title';
+
+  @service catalog;
+
+  get sortedSongs() {
+    let sortBy = this.sortBy;
+    let isDescendingSort = false;
+
+    if (sortBy.charAt(0) === '-') {
+      sortBy = this.sortBy.slice(1);
+      isDescendingSort = true;
+    }
+
+    return [...this.model.songs].sort((song1, song2) => {
+      if (song1[sortBy < song2[sortBy]]) {
+        return isDescendingSort ? 1 : -1;
+      }
+      if (song1[sortBy] > song2[sortBy]) {
+        return isDescendingSort ? -1 : 1;
+      }
+      return 0;
+    });
+  }
 
   get hashNoTitle() {
     return !this.title;
---
+++ tests/acceptance/songs-test.js
@@ -0,0 +1,101 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Acceptance | songs', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('Sort songs in various ways', async function (assert) {
+    const band = this.server.create('band', { name: 'Them Crooked Vultures' });
+    this.server.create('song', {
+      title: 'Mind Eraser, No Chaser',
+      rating: 2,
+      band,
+    });
+    this.server.create('song', {
+      title: 'Elephants',
+      rating: 4,
+      band,
+    });
+    this.server.create('song', {
+      title: 'Spinning In Daffodils',
+      rating: 5,
+      band,
+    });
+    this.server.create('song', {
+      title: 'New Fang',
+      rating: 3,
+      band,
+    });
+
+    await visit('/');
+    await click('[data-test-rr="band-link"]');
+    assert
+      .dom('[data-test-rr="song-list-item"]:first-child')
+      .hasText(
+        'Elephants',
+        'The first song is the one that comes first in the alphabet'
+      );
+    assert
+      .dom('[data-test-rr="song-list-item"]:last-child')
+      .hasText(
+        'Spinning In Daffodils',
+        'The last song is the one that comes last in the alphabet'
+      );
+
+    await click('[data-test-rr="sort-by-title-desc"]');
+    assert
+      .dom('[data-test-rr="song-list-item"]:first-child')
+      .hasText(
+        'Spinning In Daffodils',
+        'The first song is the one that comes last in the alphabet'
+      );
+    assert
+      .dom('[data-test-rr="song-list-item"]:last-child')
+      .hasText(
+        'Elephants',
+        'The last song is the one that comes first in the alphabet'
+      );
+    assert.ok(
+      currentURL().includes('s=-title'),
+      'The sort query param appears in the URL with the correct value'
+    );
+
+    await click('[data-test-rr="sort-by-rating-asc"]');
+    assert
+      .dom('[data-test-rr="song-list-item"]:first-child')
+      .hasText(
+        'Mind Eraser, No Chaser',
+        'The first song is the one that has the lowest rating'
+      );
+    assert
+      .dom('[data-test-rr="song-list-item"]:last-child')
+      .hasText(
+        'Spinning In Daffodils',
+        'The last song is the one that has the highest rating'
+      );
+    assert.ok(
+      currentURL().includes('s=rating'),
+      'The sort query param appears in the URL with the correct value'
+    );
+    await click('[data-test-rr="sort-by-rating-desc"]');
+    assert
+      .dom('[data-test-rr="song-list-item"]:first-child')
+      .hasText(
+        'Spinning In Daffodils',
+        'The first song is the one that has the highest rating'
+      );
+    assert
+      .dom('[data-test-rr="song-list-item"]:last-child')
+      .hasText(
+        'Mind Eraser, No Chaser',
+        'The last song is the one that has the lowest rating'
+      );
+    assert.ok(
+      currentURL().includes('s=-rating'),
+      'The sort query param appears in the URL with the correct value'
+    );
+  });
+});
